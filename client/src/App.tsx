fb2b9d1119ceb8146a0e2b3620af41686a1de09b
Jess <jessforster08@yahoo.com>
2025-12-11 02:59:46 -0300

    feat: added styling with tailwindcss

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,10 @@
 function App() {
   return (
-    <div>
-      <h1>Tickr</h1>
-      <p>Task management meets time tracking.</p>
+    <div className = "min-h-screen flex items-center justify-center">
+      <div className = "text-center">
+        <h1 className = "text-5x1 font-bold mb-4">Tickr</h1>
+        <p className = "text-zinc-400">Task management meets time tracking.</p>
+      </div>
     </div>
   );
 }
